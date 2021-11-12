bbb64c2ac4d19d84c93ce8c500bebcf2ed41d01b
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2021-11-12 14:05:09 -0300

    finished all games list

--- src/components/AllGamesTable.vue
+++ src/components/AllGamesTable.vue
@@ -1,24 +1,41 @@
 <template>
     <table class="table table-striped table-hover">
-  <tbody>
-    <tr class="table-active">
-      ...
-    </tr>
-    <tr>
-      ...
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2" class="table-active">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-    </table>
+    <thead>
+      <tr>
+        <th>Idx</th>
+        <th>Title</th>
+        <th>System</th>
+        <th>Finished ?</th>
+      </tr>
+    </thead>
+    <tbody>  
+      <tr v-for="game, idx in games" :key="idx">
+        <td>{{idx + 1 }}</td>
+        <td>{{game.name}}</td>    
+        <td>{{game.system}}</td>
+        <td>{{game.finished}}</td>        
+      </tr>
+    </tbody>   
+</table>
 </template>
 
 <script>
+
+import axios from 'axios';
+
 export default {
-  name: 'AllGamesTable'  
+  name: 'AllGamesTable',
+  data() {
+    return {
+      games: []      
+    }
+  },
+  created() {
+    axios.get('http://localhost:4000/all').then((resp) =>{
+      this.games = resp.data.games;      
+    });
+    
+  }  
 }
 </script>
 
