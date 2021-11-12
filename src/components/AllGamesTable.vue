9d1c70f3084bd3d6e64a776fbfc8188930815db7
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2021-11-12 12:48:25 -0300

    Implementing all games table

---
+++ src/components/AllGamesTable.vue
@@ -0,0 +1,27 @@
+<template>
+    <table class="table table-striped table-hover">
+  <tbody>
+    <tr class="table-active">
+      ...
+    </tr>
+    <tr>
+      ...
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2" class="table-active">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+  name: 'AllGamesTable'  
+}
+</script>
+
+<style scoped>
+
+</style>
