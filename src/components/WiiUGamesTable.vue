c8604bf3792c15f79bfe350caa2a5eb1d5f4f936
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2021-11-14 09:51:45 -0300

    added modal for edit a item

--- src/components/WiiUGamesTable.vue
+++ src/components/WiiUGamesTable.vue
@@ -19,24 +19,73 @@
         <td><input type="checkbox" v-model="game.FISICAL_DISC" :disabled=true></td>
         <td>
           <button type="button" class="btn btn-success btn-sm"><i class="fas fa-check"></i> Mark as Finished</button> &nbsp;
-          <button type="button" class="btn btn-primary btn-sm" @click="buttonClick('Click on Edit')"><i class="fas fa-edit"></i> Edit</button> &nbsp;
+          <button type="button" class="btn btn-primary btn-sm" @click="toggleModal"><i class="fas fa-edit"></i> Edit</button> &nbsp;
           <button type="button" class="btn btn-secondary btn-sm" @click="buttonClick('Click on Delete')"><i class="fas fa-trash-alt"></i> Delete</button>
           
         </td>
       </tr>
     </tbody>   
-</table>
+    </table>
+  <Modal @close="toggleModal" :modalActive="modalActive">
+        <template v-slot:modal-header>
+            Edit Game
+        </template>      
+        <template v-slot:modal-content>            
+            <div class="input-group mb-3  input-group-md">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon3">ID</span>
+                </div>
+                <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+            </div>
+            <div class="input-group mb-3  input-group-md">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon3">Title</span>
+                </div>
+                <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+            </div>
+            <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <div class="input-group-text">
+                <input type="checkbox" aria-label="Checkbox for following text input">
+                </div>
+            </div>
+            <input type="text" class="form-control" aria-label="Text input with checkbox" readonly placeholder="Finished?">
+            </div>
+
+            <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <div class="input-group-text">
+                <input type="checkbox" aria-label="Checkbox for following text input">
+                </div>
+            </div>
+            <input type="text" class="form-control" aria-label="Text input with checkbox" readonly placeholder="Fisical Disc?">
+            </div>
+        </template>
+        <template v-slot:modal-footer>
+            <button type="button" class="btn btn-primary">Save changes</button>
+        </template>
+    </Modal>  
 </template>
 
 <script>
 
 import axios from 'axios';
 import { useToast } from "vue-toastification";
+import Modal from "@/components/Modal.vue";
+import { ref } from "vue";
 
 export default {
   name: 'WiiUGamesTable',
   setup() {
-    // https://github.com/Maronato/vue-toastification/tree/next
+
+    const modalActive = ref(false);
+
+    const toggleModal = () => {
+      modalActive.value = !modalActive.value;
+    };
+
+
+      // https://github.com/Maronato/vue-toastification/tree/next
       // Get toast interface
       const toast = useToast();
 
@@ -50,7 +99,7 @@ export default {
       // These options will override the options defined in the "app.use" plugin registration for this specific toast
 
       // Make it available inside methods
-      return { toast }
+      return { toast, modalActive, toggleModal }
     },
   data() {
     return {
@@ -71,6 +120,9 @@ export default {
         timeout: 2000
       });
     }
+  },
+  components: {
+    Modal  
   }  
 }
 </script>
