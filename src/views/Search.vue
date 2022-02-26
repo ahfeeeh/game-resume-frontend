2a0501731fcce5588947fa0702f7715602fe1377
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-02-26 18:49:52 -0300

    fixed search page

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -3,18 +3,34 @@
       <div class="about">
           <h1>Search Page</h1>
           <p>{{ query }}</p>
+          <ul><li v-for="game, idx in games" :key="idx">{{game.title}}</li></ul>
       </div>          
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
  name: "Search Page",
  props: ['query'],
  data(){
      return {
-         route: this.$route.params
+         route: this.$route.params,
+         games: []
      }
+ },
+ methods: {
+   searchGames(query) {
+     axios.get(`http://localhost:4000/search?query=${query}`).then(data => {
+        this.games = data.data.games;     
+     });     
+   }
+ },
+ created(){
+   if (this.route.query){     
+     this.searchGames(this.route.query);
+   }
  },
  watch: {
     $route(to, from) {
