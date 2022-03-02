e33758d449ca7a3c4c0729488cce910eddfa9294
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-03-02 16:06:25 -0300

    fixes

---
+++ src/components/SearchGamesByGenreTable.vue
@@ -0,0 +1,29 @@
+<template>
+    <table class="table table-striped table-hover">
+    <thead>
+      <tr>        
+        <th>AppID</th>
+        <th>Title</th>        
+        <th>System</th>       
+      </tr>
+    </thead>
+    <tbody>  
+      <tr v-for="game, idx in games" :key="idx">        
+        <td>{{game.app_id }}</td>
+        <td>{{game.title}}</td>            
+        <td>{{game.system}}</td>        
+      </tr>
+    </tbody>   
+</table>
+</template>
+
+<script>
+export default {
+  name: 'SearchGamesByGenreTable',
+  props: ["games"]  
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TotalGenresTableLink.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <table class="table table-sm table-dark">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Genre</th>
+          <th scope="col">Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(d, idx) in data" :key="idx">
+          <th scope="row">{{ idx }}</th>
+          <td><a :href="`http://localhost:8080/genre/${d.genre}`">{{ d.genre }}</a></td>
+          <td>{{ d.total }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TotalGenresTableLink",
+  props: {
+    data: Array
+  }
+};
+</script>
+
+<style>
+</style>
