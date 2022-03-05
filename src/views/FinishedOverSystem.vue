612fc65d1e2543d8387551dc3f488184b33027a9
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-03-05 11:13:47 -0300

    fixes

--- src/views/FinishedOverSystem.vue
+++ src/views/FinishedOverSystem.vue
@@ -2,20 +2,31 @@
     <div class="container py-4" style="margin-top: 50px">
         <h1>Finished over System</h1>
         
-        <div class="card text-white bg-dark mb-3" v-for="stat,idx in stats" :key="idx">
-            <div class="card-header">{{stat.system}}</div>
-                <div class="card-body">
-                    <h5 class="card-title">Total of Games: {{stat.total_of_games}}</h5>
-                    <p class="card-text">Games Finished: {{stat.finished_cnt}}</p>
-                    
-                    <div class="progress">
-                        <div class="progress-bar progress-bar-striped progress-bar-animated" 
-                            role="progressbar" :aria-valuenow="stat.percentual" aria-valuemin="0" aria-valuemax="100" :style="{width: `${Math.round(stat.percentual)}%`}">
-                        {{`${Math.round(stat.percentual)}%`}}
-                        </div>
-                    </div>
-                </div>
+        <div class="container-fluid">
+            <div class="row row-cols-3">
+                <div class="col" v-for="stat,idx in stats" :key="idx">
+                            <div class="card text-white bg-dark mb-3" style="max-width:100%">
+                                <div class="card-header">{{stat.system}}</div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Total of Games: {{stat.total_of_games}}</h5>
+                                        <p class="card-text">Games Finished: {{stat.finished_cnt}}</p>
+                                        
+                                        <div class="progress">
+                                            <div class="progress-bar progress-bar-striped progress-bar-animated" 
+                                                role="progressbar" :aria-valuenow="stat.percentual" aria-valuemin="0" aria-valuemax="100" :style="{width: `${Math.round(stat.percentual)}%`}">
+                                            {{`${Math.round(stat.percentual)}%`}}
+                                            </div>
+                                        </div>
+                                    </div>
+                            </div>
+                </div>                
+            </div>
+
+
+
+
         </div>
+        
 
         
     </div>    
