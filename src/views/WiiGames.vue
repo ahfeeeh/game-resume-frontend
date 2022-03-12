4bb2e9f1293d0ec7cbb993f05655105de45bd018
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2022-03-12 15:15:19 -0300

    fixed wii

--- src/views/WiiGames.vue
+++ src/views/WiiGames.vue
@@ -30,12 +30,21 @@
                 </div>
                 <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3" v-model="newItem.app_id">
             </div>
+
+            <div class="input-group mb-3  input-group-md">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon3">SystemId</span>
+                </div>
+                <input type="text" readonly class="form-control" id="basic-url" aria-describedby="basic-addon3" v-model="newItem.system_id">
+            </div>
+            
             <div class="input-group mb-3  input-group-md">
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="basic-addon3">Title</span>
                 </div>
                 <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3" v-model="newItem.title">
             </div>
+            
             <div class="input-group mb-3">
             <div class="input-group-prepend">
                 <div class="input-group-text">
@@ -45,6 +54,26 @@
             <input type="text" class="form-control" aria-label="Text input with checkbox" readonly placeholder="Finished?">
             </div>
 
+            
+            <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <div class="input-group-text">
+                <input type="checkbox" aria-label="Checkbox for following text input" v-model="newItem.collection">
+                </div>
+            </div>
+            <input type="text" class="form-control" aria-label="Text input with checkbox" readonly placeholder="Collection?">
+            </div>
+
+            <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <div class="input-group-text">
+                <input type="checkbox" aria-label="Checkbox for following text input" v-model="newItem.genuine">
+                </div>
+            </div>
+            <input type="text" class="form-control" aria-label="Text input with checkbox" readonly placeholder="Genuine?">
+            </div>
+
+
             <div class="input-group mb-3">
             <div class="input-group-prepend">
                 <div class="input-group-text">
@@ -53,6 +82,7 @@
             </div>
             <input type="text" class="form-control" aria-label="Text input with checkbox" readonly placeholder="Fisical Disc?">
             </div>
+
         </template>
         <template v-slot:modal-footer>
             <button type="button" class="btn btn-primary" @click="saveGame(newItem)">Save changes</button>
@@ -102,16 +132,13 @@ export default {
   data() {
     return {      
       isLoading: true,
-      newItem: {app_id: "", title: "", finished: null, fisical_disc: null, collection: null, genuine:null, table: 'wii'}      
+      newItem: {app_id: "", system_id: 5, title: "", finished: null, collection: null, genuine: null, fisical_disc: null, table: 'wii'}      
     }
   },
   methods: {
     saveGame(payload) {  
-      /*FIX ME*/
-      payload.collection = false;
-      payload.genuine = false;
       this.store.dispatch('saveGame', { payload, toast: this.toast, toggleModal: this.toggleModal })  
-      this.newItem = {app_id: "", title: "", finished: null, fisical_disc: null, collection: null, genuine:null, table: 'wii'}            
+      this.newItem = {app_id: "", system_id: 5, title: "", finished: null, collection: null, genuine: null, fisical_disc: null, table: 'wii'}            
     }
   }
 }
